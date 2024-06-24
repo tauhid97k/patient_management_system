07d28a2a2ede3644cc17b67185e540be50e6f072
tauhid97k <tauhid.coder@example.com>
2024-06-24 22:48:02 +0600

    feat: :fire: simple states on dashboard overview page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return inertia('Dashboard/Overview');
+        $userCount = User::count();
+        $patientCount = Patient::count();
+        $rolesCount = Role::count();
+
+        return inertia('Dashboard/Overview', ['data' => [
+            'users' => $userCount,
+            'patients' => $patientCount,
+            'roles' => $rolesCount
+        ]]);
     }
 }
--- resources/js/Pages/Dashboard/Overview.jsx
+++ resources/js/Pages/Dashboard/Overview.jsx
@@ -1,8 +1,34 @@
-const OverviewPage = () => {
+import { Link } from "@inertiajs/react";
+
+const OverviewPage = ({ data }) => {
     return (
         <>
             <div className="flex items-center justify-between mb-5">
                 <h2 className="text-2xl font-medium">Overview</h2>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                <Link
+                    href={route("users.index")}
+                    className="text-2xl p-4 rounded-md border border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800 flex justify-between"
+                >
+                    <h2>Users</h2>
+                    <span>{data.users}</span>
+                </Link>
+                <Link
+                    href={route("patients.index")}
+                    className="text-2xl p-4 rounded-md border border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800 flex justify-between"
+                >
+                    <h2>Patients</h2>
+                    <span>{data.patients}</span>
+                </Link>
+                <Link
+                    href={route("rolePermissions.index")}
+                    className="text-2xl p-4 rounded-md border border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800 flex justify-between"
+                >
+                    <h2>Roles</h2>
+                    <span>{data.roles}</span>
+                </Link>
             </div>
         </>
     );
